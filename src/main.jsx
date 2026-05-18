df5f3be7ce1b5862463185d7e397d538dd427b29
jiuwanlingjiuqian <18975852192@163.com>
2026-05-18 22:57:12 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AppProvider } from './AppContext';
-import FloatingBg from './components/FloatingBg';
 import './styles/theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AppProvider>
-        <FloatingBg />
         <App />
       </AppProvider>
     </BrowserRouter>
